d1088ced52f35aa92b5fb5e6eee1f1fcc3138eb9
johnuary <jvligazon@gmail.com>
2020-10-22 10:35:00 +0800

    react-setup

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import React from 'react'
+import Link from 'next/link'
+import {} from '@chakra-ui/core'
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Noemi's Cheesy Beef Lasagna</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </>
+  )
+}
+
+export default Home
