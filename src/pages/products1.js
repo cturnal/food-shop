d31e56302e60809ad983243b756f55b3d75a643d
johnuary <jvligazon@gmail.com>
2020-10-30 20:25:43 +0800

    deploy-food-shop

--- src/pages/products1.js
+++ src/pages/products1.js
@@ -2,10 +2,32 @@ import { Box, Flex } from '@chakra-ui/core'
 import Container from '../components/Container'
 import Cards from '../components/Cards'
 
+const prefix = '/food-shop/'
+
+const addToCart = {
+  imageSrc: '/lasagna.jpg',
+}
+
 function Products1() {
   return (
     <>
-      <Container cName='add-to-cart'>
+      <Container
+        cName='add-to-cart'
+        style={{
+          backgroud:
+            'linear-gradient(#fffaf0, #fffaf050 50%, #fffaf0)' +
+            ' url(' +
+            prefix +
+            addToCart.imageSrc +
+            ')' +
+            'fixed',
+          backgroudSize: 'cover',
+          width: '100%',
+          paddingBottom: '150px',
+          position: 'relative',
+          maxW: '100%',
+        }}
+      >
         <Box m='auto'>
           <Flex
             flexDirection={{
